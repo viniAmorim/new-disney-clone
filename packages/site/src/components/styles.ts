e0ed50b6f0042a23ec5f1f9d89cf39e08e902ef5
viniAmorim <amorimm.viniciuss@gmail.com>
2022-01-20 14:47:42 -0300

    Change background content color

--- packages/site/src/components/styles.ts
+++ packages/site/src/components/styles.ts
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 export const Content = styled.div`
   background: ${({ theme }) => theme.colors['gray-500']};
   height: 100%;
+  background-color: #13151f;
 `;
 
 export const Container = styled.div`
   padding-top: 32px;
-  background-color: #13151f;
 `;
